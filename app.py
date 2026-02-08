8e0e7f8d803ace7537d2568a15fd7b2a64e20e8c
PhijamNongthanganba1 <nongthanganba.phijam@gmail.com>
2026-02-09 05:29:31 +0530

    adding more file

--- app.py
+++ app.py
@@ -1153,8 +1153,12 @@ def configure_debug():
 
 
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+
+# if __name__ == '__main__':
     
-    app.run(debug=True, port=5000)
+#     app.run(debug=True, port=5000)
